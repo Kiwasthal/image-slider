63569d6695416ff6f90a5e896f76ceecd4055adf
Steerpike <imthekoul@gmail.com>
2022-05-02 02:55:29 +0300

    Added assets and basic js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hey');
+let img = document.createElement('img');
+img.classList.add('todolist');
+img.src = './assets/todolist.gif';
+let imgContainer = document.querySelector('.image-container');
+
+imgContainer.appendChild(img);
+
+function start() {
+  setTimeout(function () {
+    start();
+  }, 3000);
+}
+
+start();
